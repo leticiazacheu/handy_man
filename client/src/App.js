78aaefc73193f363fb637e7ad6915d8c0534cd7a
John Doe <paytonanthony2020@gmail.com>
2021-06-17 20:21:41 -0600

    updated Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/shared/Home';
-//import About from './components/shared/About';
+import About from './components/shared/About';
+import Nabar from './components/shared/Navbar';
 // nav routes
   const App = () => (
     <>
+      <Nabar />
       <Switch>      
         <Route exact path="/" component={Home} />
         <Route exact path="/home" component={Home} />
-       {/* <Route exact path="/about" component={About} /> */}
+       <Route exact path="/about" component={About} />
       </Switch>
      
     </>
