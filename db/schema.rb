2049dfd272405796e81503405cf9bff277c7f74d
John Doe <paytonanthony2020@gmail.com>
2021-06-17 20:53:51 -0600

    Fixed seed and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_003255) do
+ActiveRecord::Schema.define(version: 2021_06_18_024821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,12 +26,12 @@ ActiveRecord::Schema.define(version: 2021_06_18_003255) do
   end
 
   create_table "services", force: :cascade do |t|
-    t.string "type"
     t.boolean "complete"
     t.integer "rating"
     t.bigint "worker_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
     t.index ["worker_id"], name: "index_services_on_worker_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+3.times do
+worker = Worker.create(
+    first_name: Faker::Artist.name,
+    last_name: Faker::Hacker.abbreviation,
+    specialty: Faker::Hacker.noun,
+  )
+
+  5.times do
+    ser = Service.create(
+      title: Faker::Science.element,
+      complete: Faker::Boolean.boolean,
+      rating: Faker::Number.between(from: 1, to: 10),
+      worker_id: worker.id,
+    )
+    2.times do
+      Comment.create(
+        author: Faker::FunnyName.name,
+        subject: Faker::Educator.subject,
+        body: Faker::Markdown.emphasis,
+        service_id: ser.id
+      )
+    end
+  end
+end
+
+puts "Data Seeded"
