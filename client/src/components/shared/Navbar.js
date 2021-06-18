0ac278fba78100c8e39beb79adcbc12e9f4db08b
Steph Mahnke <mahnkes1@msu.edu>
2021-06-17 20:11:30 -0600

    added worker js

---
+++ client/src/components/shared/Navbar.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <>
+    <ul>
+      <li>
+        <Link to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/about">
+          About
+        </Link>
+      </li>
+      <li>
+        <Link to="/workers">
+          Workers
+        </Link>
+      </li>
+    </ul>
+  </>
+)
+export default Navbar;
---
+++ client/src/components/workers/Worker.js
@@ -0,0 +1,13 @@
+import Services from "../posts/Services";
+
+const Worker = ({ id, first_name, last_name, specialty, updateWorker, deleteWorker }) => {
+  return(
+    <>
+      <h1>{first_name} {last_name}</h1>
+      <h3>Specialty: {specialty}</h3>
+      <button onClick={ () => deleteWorker(id) }>Delete Worker</button>
+      <Services workerId={id} />
+    </>
+  )
+}
+export default Worker;
